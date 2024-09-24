3a44c879e3905b6546f3c857cfe8bfba6dfdca78
PolMikeYap <mike.yap81@gmail.com>
2024-09-24 08:57:48 +0800

    initial commit

---
+++ Assets/Scripts/main.js
@@ -0,0 +1,10 @@
+console.log("Hello World!")
+console.log("Welcome Web Dev 101")
+console.log("Batch 9")
+
+const pol = "Pol Yap"
+const abc = "This is the Value of ABC"
+const x = 2
+const y = 5
+
+console.log(x + y)
